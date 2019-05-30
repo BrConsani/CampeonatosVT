76e7a629bd1b6814d6d78e5ef8154116a725e488
Bruno Consani <brunihxd@hotmail.com>
2019-05-30 17:25:16 -0300

    Implemented remove step

--- app/src/main/java/br/com/beirario/campeonatosvt/adapters/StepAdapter.java
+++ app/src/main/java/br/com/beirario/campeonatosvt/adapters/StepAdapter.java
@@ -1,19 +1,24 @@
 package br.com.beirario.campeonatosvt.adapters;
 
 import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import java.util.List;
 
 import br.com.beirario.campeonatosvt.models.RaceSteps;
+import br.com.beirario.campeonatosvt.ui.Views;
+import br.com.beirario.campeonatovt.R;
 
 public class StepAdapter extends OneLineAdapter<RaceSteps> {
 
     private Context context;
+    private List<RaceSteps> steps;
 
     public StepAdapter(Context context, List<RaceSteps> items) {
         super(items);
         this.context = context;
+        this.steps = items;
     }
 
     @Override
@@ -23,6 +28,12 @@ public class StepAdapter extends OneLineAdapter<RaceSteps> {
 
     @Override
     public boolean onLongClick(View view, int i) {
+        Views.DialogBuilder builder = new Views.DialogBuilder((AppCompatActivity) context, R.string.body1_remove_step);
+        builder.setPositiveButton(R.string.btn_remove, ((dialog, which) -> {
+            steps.remove(i);
+            notifyItemRemoved(i);
+        }));
+        builder.buildDialog().show();
         return false;
     }
 }
