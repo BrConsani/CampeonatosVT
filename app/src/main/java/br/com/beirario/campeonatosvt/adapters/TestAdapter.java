d0da9d3bd3afae8333a63ba56d1479cca0fa10e1
Bruno Consani <brunihxd@hotmail.com>
2019-06-03 11:42:28 -0300

    Changed how ListenerInterface works

--- app/src/main/java/br/com/beirario/campeonatosvt/adapters/TestAdapter.java
+++ app/src/main/java/br/com/beirario/campeonatosvt/adapters/TestAdapter.java
@@ -1,27 +1,38 @@
 package br.com.beirario.campeonatosvt.adapters;
 
 import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import br.com.beirario.campeonatosvt.models.Step;
 import br.com.beirario.campeonatosvt.models.Test;
+import br.com.beirario.campeonatosvt.ui.Views;
+import br.com.beirario.campeonatovt.R;
 
 public class TestAdapter extends OneLineAdapter<Test> {
 
-    private Context context;
+    private AppCompatActivity context;
+    private Step step;
 
-    public TestAdapter(Context context, Step step) {
+    public TestAdapter(AppCompatActivity context, Step step) {
         super(step.getTests());
         this.context = context;
+        this.step = step;
     }
 
     @Override
-    public void onClick(View view, int i) {
+    public void onClick(View view, Test o) {
 
     }
 
     @Override
-    public boolean onLongClick(View view, int i) {
+    public boolean onLongClick(View view, Test o) {
+        Views.DialogBuilder builder = new Views.DialogBuilder(context, R.string.body1_remove_step);
+        builder.setPositiveButton(R.string.btn_remove, ((dialog, which) -> {
+            step.removeTest(o);
+            notifyItemRemoved(step.getTests().indexOf(o));
+        }));
+        builder.buildDialog().show();
         return false;
     }
 }
